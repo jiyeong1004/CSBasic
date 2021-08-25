a2540cbdc690d3d8801bedbd128220566d64470e
s2019s28 <s2019s28@e-mirim.hs.kr>
2021-08-25 10:05:16 +0900

    #31 MessageBox 활용 실습

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,37 @@ namespace FormPrac
         {
             InitializeComponent();
         }
+
+        private void btnMessageBox1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            DialogResult result = DialogResult.None;
+
+            if (btn.Name == "btnMessageBox1")
+            {
+                result = MessageBox.Show("안녕하세요~");  // 말
+            }
+            else if (btn.Name == "btnMessageBox2")
+            {
+                result = MessageBox.Show("안녕하세요~", "격하게 환영인사");  // 제목
+            }
+            else if (btn.Name == "btnMessageBox3")
+            {
+                result = MessageBox.Show("안녕하세요~", "격하게 환영인사",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Information);  // MessageBox버튼, 아이콘
+            }
+            switch (result)
+            {
+                case DialogResult.OK:
+                    MessageBox.Show("나도 반가워요~ 😊");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("나도 안반가워요~ 😂");
+                    break;
+
+            }
+
+        }
     }
 }
